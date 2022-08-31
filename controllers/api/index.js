effce4340e318a3e22f6909a57223a6f9167f7c0
Hoefer <ningboninja@gmail.com>
2022-08-31 13:07:31 -0700

    fixing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 // const coachRoutes = require('./coachRoutes.js');
 
-// const clientRoutes = require('./clientRoutes.js');
+const clientRoutes = require('./clientRoutes.js');
 // const exerciseRoutes = require('./exerciseRoutes.js');
 
 // router.use('/coaches', coachRoutes);
-// router.use('/clients', clientRoutes);
+router.use('/clients', clientRoutes);
 // router.use('/exercises', exerciseRoutes);
 
 module.exports = router;

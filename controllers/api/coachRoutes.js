a3e49fce2a233b687ef04f5a0f7eb2e5b110f690
Hoefer <ningboninja@gmail.com>
2022-08-31 13:11:16 -0700

    psuh

---
+++ controllers/api/coachRoutes.js
@@ -0,0 +1,14 @@
+// const router = require('express').Router();
+// const { Client, Coach, } = require('../../models');
+
+// router.get("/", (req, res) => {
+//   Coach.findAll({
+//     include: [Client,]
+//   }).then(data => {
+//     res.json(data)
+//   }).catch(err => {
+//     res.status(500).json({ msg: "womp womp", err })
+//   })
+// })
+
+// module.exports = router;
